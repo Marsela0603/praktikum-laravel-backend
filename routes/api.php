b0c84b6e64a6dd5258d518d6c4b11761cf744e66
Marsela0603 <marselasela3554@gmail.com>
2024-10-29 22:59:16 +0700

    first commit

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnimalController;
+use App\Http\Controllers\StudentController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,11 +13,15 @@ Route::get('/user', function (Request $request) {
 Route::get('/users', [AnimalController::class, 'index']);
 Route::post('/users/store', [AnimalController::class, 'store']);
 
-
+//animals
 Route::get('/animals', [AnimalController::class, 'index']);
-
 Route::post('/animals', [AnimalController::class, 'store']);
-
 Route::put('/animals/{id}', [AnimalController::class, 'update']);
-
 Route::delete('/animals/{id}', [AnimalController::class, 'destroy']);
+
+//students
+Route::get('/students', [StudentController::class, 'index']);
+Route::post('/students', [StudentController::class, 'store']);
+Route::put('/students/{id}', [StudentController::class, 'update']); 
+Route::delete('/students/{id}', [StudentController::class, 'destroy']); 
+
